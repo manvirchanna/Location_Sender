bcc934fb9a34fe963e6b71b6ae8196d91b8bc670
Guneet Kaur <43650631+guneet99@users.noreply.github.com>
2020-10-02 10:02:08 +0530

    Update location_sender.py

--- location_sender.py
+++ location_sender.py
@@ -1,13 +1,17 @@
 import urllib3
 import json
+import requests
+from twilio.rest import Client #for text messaging.
+
 http = urllib3.PoolManager()
 r = http.request('GET', 'http://ipinfo.io/json')
 data = json.loads(r.data.decode('utf-8'))
 city=data['city']
 loc=data['loc']
 print(city,loc)
-from twilio.rest import Client #for text messaging.
-client = Client("YOUR TWILO SSID", "AUTH TOKEN")
+sid= "your twilio account sid"
+token= "your twilio auth token"
+client = Client(sid, token)
 client.messages.create(to="Receiver contact number",
                        from_="Sender contact number",
 body="Hello,I am in  "+city+"   and my location cordinates are  " +loc)
